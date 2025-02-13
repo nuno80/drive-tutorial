a84a55b2962b98fc7119345d68c5f3ade93c23b7
nuno.80.al <nuno.80.al@gmail.com>
2025-02-13 10:40:31 +0000

    build: Add JSDoc comment to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
